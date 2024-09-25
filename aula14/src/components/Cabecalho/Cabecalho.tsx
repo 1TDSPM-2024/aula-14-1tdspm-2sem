616d07987351d410548c9fdbbb463c114998b0f0
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-25 09:00:45 -0300

    Trabalhando com tailwind globals.

--- aula14/src/components/Cabecalho/Cabecalho.tsx
+++ aula14/src/components/Cabecalho/Cabecalho.tsx
@@ -2,7 +2,7 @@ import Menu from "../Menu/Menu";
 
 export default function Cabecalho() {
     return (
-        <header>
+        <header className="cabecalho">
             <h1>Cabecalho</h1>
             <Menu />
         </header>
