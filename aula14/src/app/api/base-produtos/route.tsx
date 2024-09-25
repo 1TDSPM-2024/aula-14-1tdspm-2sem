6f304cae979efcd8d86151c8b5f1b5b4405a5203
nyxtais <nyxtais@gmail.com>
2024-09-25 09:48:59 -0300

    utilizando rotas API com GET-ALL router-randlers

--- aula14/src/app/api/base-produtos/route.tsx
+++ aula14/src/app/api/base-produtos/route.tsx
@@ -1,14 +1,12 @@
+import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
 
-const dados = [
-    { id: 1, nome: "napolitano", qtd: 1000 },
-    { id: 2, nome: "vinho", qtd: 2000 },
-    { id: 3, nome: "abacaxi", qtd: 1000 },
-    { id: 4, nome: "morango", qtd: 1000 },
-    { id: 5, nome: "mexerica", qtd: 1000 },
-    { id: 6, nome: "creme", qtd: 10000 },
-];
 
 export async function GET(){
-    return NextResponse.json(dados);
+
+    const file = await fs.readFile(process.cwd() + "/src/data/base.json", "utf-8");
+
+    //parsear o arquivo
+    const produtos = JSON.parse(file);
+    return NextResponse.json(produtos);
 }
