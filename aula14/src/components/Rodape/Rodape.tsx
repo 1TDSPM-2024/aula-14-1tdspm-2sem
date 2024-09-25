b405898106bdf1b94769ac2b6c054201959b7454
biancavitoria15 <bianca1.vitoria@gmail.com>
2024-09-25 09:01:59 -0300

    trabalhando com tailwind globals

--- aula14/src/components/Rodape/Rodape.tsx
+++ aula14/src/components/Rodape/Rodape.tsx
@@ -2,7 +2,7 @@ export default function Rodape() {
     return (
         <footer>
             <h2>Rodapé</h2>
-            <p>Desenvolvido <span>Rei Luizinho</span></p>
+            <p>Desenvolvido <span>bianca</span></p>
         </footer>
     )
 }
