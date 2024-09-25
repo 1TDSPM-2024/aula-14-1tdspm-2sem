132d78cd55a3a379cb9786fe49b8b4f45067f200
IsabellaMedeiros1 <user.email>
2024-09-25 09:49:47 -0300

    json

--- aula14/src/components/Cabecalho/Cabecalho.tsx
+++ aula14/src/components/Cabecalho/Cabecalho.tsx
@@ -3,7 +3,7 @@ import Menu from "../Menu/Menu";
 export default function Cabecalho() {
     return (
         <header className="cabecalho">
-            <h1>Cabecalho</h1>
+            <h1 className="title-cabecalho">Cabecalho</h1>
             <Menu />
         </header>
     )
