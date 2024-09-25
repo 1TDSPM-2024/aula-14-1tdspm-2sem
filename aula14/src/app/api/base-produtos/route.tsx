741cfa8975e7cf25625436558d3054b46235b92c
nyxtais <nyxtais@gmail.com>
2024-09-25 09:30:45 -0300

    passando 'dados' json

--- aula14/src/app/api/base-produtos/route.tsx
+++ aula14/src/app/api/base-produtos/route.tsx
@@ -1,8 +1,14 @@
 import { NextResponse } from "next/server";
 
-
-
+const dados = [
+    { id: 1, nome: "napolitano", qtd: 1000 },
+    { id: 2, nome: "vinho", qtd: 2000 },
+    { id: 3, nome: "abacaxi", qtd: 1000 },
+    { id: 4, nome: "morango", qtd: 1000 },
+    { id: 5, nome: "mexerica", qtd: 1000 },
+    { id: 6, nome: "creme", qtd: 10000 },
+];
 
 export async function GET(){
-    return NextResponse.json({message:"Hello World!"});
+    return NextResponse.json(dados);
 }
