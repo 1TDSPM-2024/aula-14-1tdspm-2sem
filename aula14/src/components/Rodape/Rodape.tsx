eb4076c71f0dffbc54f75a48f334745b89e0d59b
StaniukaitisPaula <paulablesa6@gmail.com>
2024-09-25 09:02:34 -0300

    trabalhando tai globals

--- aula14/src/components/Rodape/Rodape.tsx
+++ aula14/src/components/Rodape/Rodape.tsx
@@ -1,6 +1,6 @@
 export default function Rodape() {
     return (
-        <footer>
+        <footer className="">
             <h2>Rodapé</h2>
             <p>Desenvolvido <span>Rei Luizinho</span></p>
         </footer>
